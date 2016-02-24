28cf1362e8029a5b173664a2496315d9559d303f
Hind Almushigih <almushig002@knights.gannon.edu>
2016-02-24 12:23:12 -0500

    add the UItesting

--- AutoSchedulerUITests/AutoSchedulerUITests.m
+++ AutoSchedulerUITests/AutoSchedulerUITests.m
@@ -32,9 +32,74 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testTheloginAndTheMenueAndViewAndAddNewProject{
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.textFields[@"UserName"] typeText:@"admin"];
+    
+    XCUIElement *passwordSecureTextField = app.secureTextFields[@"Password"];
+    [passwordSecureTextField tap];
+    [passwordSecureTextField typeText:@"admin"];
+    [app.buttons[@"Login"] tap];
+    
+    XCUIElement *revealIconButton = app.navigationBars[@"HomeView"].buttons[@"reveal icon"];
+    [revealIconButton tap];
+    [revealIconButton tap];
+    [revealIconButton tap];
+    
+    XCUIElementQuery *tablesQuery2 = app.tables;
+    XCUIElementQuery *tablesQuery = tablesQuery2;
+    [tablesQuery.staticTexts[@"Home"] tap];
+    [app.staticTexts[@"Welcom: Redmine Admin"] tap];
+    [app.staticTexts[@"Email: autoschedulerapp@gmail.com"] tap];
+    [app.staticTexts[@"Last connection: 2016-02-24T17:16:11Z"] tap];
+    [revealIconButton tap];
+    
+    XCUIElement *projectsStaticText = tablesQuery.staticTexts[@"Projects"];
+    [projectsStaticText tap];
+    [[tablesQuery2.cells containingType:XCUIElementTypeStaticText identifier:@"test"].staticTexts[@"View Project..."] tap];
+    
+    XCUIElement *element2 = [[[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element;
+    XCUIElement *element = [element2 childrenMatchingType:XCUIElementTypeOther].element;
+    [[[[element childrenMatchingType:XCUIElementTypeStaticText] matchingIdentifier:@"test"] elementBoundByIndex:0] tap];
+    [[[[element childrenMatchingType:XCUIElementTypeStaticText] matchingIdentifier:@"test"] elementBoundByIndex:1] tap];
+    [[[[element childrenMatchingType:XCUIElementTypeStaticText] matchingIdentifier:@"2016-02-17T23:07:02Z"] elementBoundByIndex:0] tap];
+    [[[[element childrenMatchingType:XCUIElementTypeStaticText] matchingIdentifier:@"2016-02-17T23:07:02Z"] elementBoundByIndex:1] tap];
+    [[[[app.navigationBars[@"Project Details"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Back"] elementBoundByIndex:0] tap];
+    
+    XCUIElement *projectstableviewNavigationBar = app.navigationBars[@"ProjectsTableView"];
+    XCUIElement *addButton = projectstableviewNavigationBar.buttons[@"Add"];
+    [addButton tap];
+    
+    XCUIElement *newprojectviewNavigationBar = app.navigationBars[@"NewProjectView"];
+    [newprojectviewNavigationBar.buttons[@"Cancel"] tap];
+    [addButton tap];
+    
+    XCUIElement *textField = [[element2 childrenMatchingType:XCUIElementTypeTextField] elementBoundByIndex:0];
+    [textField tap];
+    [textField typeText:@"TestProject"];
+    
+    XCUIElement *textView = [element2 childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"This is to test the adding feature for project in the iOS app for redmine web-app."];
+    
+    XCUIElement *textField2 = [[element2 childrenMatchingType:XCUIElementTypeTextField] elementBoundByIndex:1];
+    [textField2 tap];
+    [textField2 tap];
+    [textField2 typeText:@"thisID"];
+    [newprojectviewNavigationBar.buttons[@"Save"] pressForDuration:1.6];
+    XCUIElement *revealIconButton2 = projectstableviewNavigationBar.buttons[@"reveal icon"];
+    [revealIconButton2 tap];
+    [projectsStaticText tap];
+    [revealIconButton2 tap];
+    [revealIconButton2 tap];
+    [tablesQuery.staticTexts[@"Issues"] tap];
+    [[[[app.navigationBars[@"Issue Details"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Back"] elementBoundByIndex:0] tap];
+    
 }
+
 
 @end
